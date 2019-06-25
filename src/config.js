43cf2e1a37cf1eff917f4669a98a809130b68d2f
Sarah Burgess <sarahburgess287@gmail.com>
2019-06-24 20:52:17 -0700

    Updating .env url and comment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // export const API_BASE_URL =
 //     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-// console.log(process.env);
+console.log(process.env);
+console.log('got it!')
 // note;  added /api to .env,and now it's using localhost again
 
 module.exports = {
